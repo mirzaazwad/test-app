8fea4572d1544f64bdf8b6690b689ae5e3a26893
Mirza Mohammad Azwad <50703605+mirzaazwad@users.noreply.github.com>
2023-07-06 20:26:59 +0600

    updates #5

--- code/src/config/firestore.ts
+++ code/src/config/firestore.ts
@@ -5,13 +5,13 @@ import {GoogleAuthProvider,getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: import.meta.env._API_KEY,
-  authDomain: import.meta.env._AUTH_DOMAIN,
-  projectId: import.meta.env._PROJECT_ID,
-  storageBucket: import.meta.env._STORAGE_BUCKET,
-  messagingSenderId: import.meta.env._MESSAGE_SENDER_ID,
-  appId: import.meta.env._APP_ID,
-  measurementId: import.meta.env._MEASUREMENT_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGE_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 export const app = initializeApp(firebaseConfig);
