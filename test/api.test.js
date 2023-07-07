fe803959a60f5371bfcb8cc5305ae6dd481487c1
curryfizz <nawsheen141@gmail.com>
2023-07-07 23:28:47 +0600

    wat is happening ;-;

---
+++ test/api.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+
+describe('Auth Routes', () => {
+  
+
+  test('GET /auth/register should render the register view', async () => {
+    const response = "123";
+    expect(response).toBe("123");
+    // Add additional assertions as needed
+  });
+});
