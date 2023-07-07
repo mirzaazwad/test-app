964072172b919a5202c822cea2d370bf716ba11e
curryfizz <nawsheen141@gmail.com>
2023-07-07 16:16:41 +0600

    changed output

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!!!');
+  res.send('Ken KAAJ KORTESENA AAAAAAAAAAAAAAAA 😭😢');
 });
 
 app.get('/hello',(req,res)=>{
