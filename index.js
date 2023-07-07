63e057bde22682104127200c470bd52946037111
curryfizz <nawsheen141@gmail.com>
2023-07-07 17:24:20 +0600

    added port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
   res.send('Ken KAAJ KORTESENA AAAAAAAAAAAAAAAA 😭😢');
