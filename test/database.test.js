7be2eace348415782f6fa9e7773b9498b02b2c36
curryfizz <nawsheen141@gmail.com>
2023-07-07 16:13:42 +0600

    basic stuff

---
+++ test/database.test.js
@@ -0,0 +1,24 @@
+const request = require('supertest');
+const { app, server } = require('../index');
+
+describe('API Routes', () => {
+  it('Test #1', async () => {
+      const response = await request(app).get('/');
+      expect(response.status).toBe(200);
+      expect(response.text).toBe('Hello World!!!');
+  });
+
+  it('Test #2', async () => {
+      const response = await request(app).get('/hello');
+      expect(response.status).toBe(200);
+      expect(response.text).toBe('Bye World');
+  });
+  it('Test #3', async () => {
+    const response = await request(app).get('/nafisa');
+    expect(response.status).toBe(200);
+    expect(response.text).toBe('Nafisa is successful');
+});
+});
+afterAll((done) => {
+  server.close(done);
+});
